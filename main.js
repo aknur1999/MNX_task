cc9dba7d5963f8cfa60e721719eb8d124b3d21ee
aknur1999 <kae.aknur@gmail.com>
2023-12-20 18:29:41 +0900

    part two + test cases

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ function isPalindrome(str) {
     }
     return true;
 }
+
+function isPalindromeUpdated(str) {
+    str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome("RAcecar"));
+console.log(isPalindromeUpdated("nurses run")); 
